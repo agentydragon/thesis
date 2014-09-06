cdec06d2f63d3a62ffcf001749caac9e963cff82
Michal Pokorný <pok@rny.cz>
2014-09-07 01:05:04 +0200

    Remove pointer arithmetic from toycrypt algorithm

--- test_hash/toycrypt.c
+++ test_hash/toycrypt.c
@@ -1,17 +1,16 @@
 #include "toycrypt.h"
 
 static uint32_t oneway_fn(uint32_t a, uint64_t b) {
-	uint32_t x = a, y = *((uint32_t*)(&b)), z = *((uint32_t*)(&b) + 1);
-	return (x << 24) + (y << 16) + (z << 8) + x;
+	// A variant of the FNV hashing function.
+	const uint32_t basis = 2166136261LL, prime = 16777619LL;
+	return (((basis * prime) ^ a) * prime) ^ b;
 }
 
 static uint64_t one_round(uint64_t message, uint64_t key) {
-	uint32_t L1 = *(uint32_t*)(&message), L2 = *((uint32_t*)(&message) + 1);
-	uint32_t R1 = L2 ^ oneway_fn(L1, key) , R2 = L1;
-	uint64_t out;
-	*(uint32_t*)(&out) = R1;
-	*((uint32_t*)(&out) + 1) = R2;
-	return out;
+	uint32_t L1 = (message & 0xFFFFFFFF00000000LL) >> 32LL,
+		 L2 = (message & 0x00000000FFFFFFFFLL);
+	uint32_t R1 = L2 ^ oneway_fn(L1, key), R2 = L1;
+	return (((uint64_t) R1) << 32LL) | (uint64_t) R2;
 }
 
 uint64_t toycrypt(uint64_t message, uint64_t key) {
