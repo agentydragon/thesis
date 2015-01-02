8700fb56edfd4c87dd60aaab34c15a14516154a3
Michal Pokorný <pok@rny.cz>
2015-01-03 00:18:13 +0100

    Tests for exact_log2

--- src/math/test.c
+++ src/math/test.c
@@ -1,13 +1,25 @@
 #include "math.h"
 #include <assert.h>
 
-void test_math() {
+static void test_floor_log2() {
 	assert(floor_log2(1) == 0);
 	assert(floor_log2(2) == 1);
 	assert(floor_log2(3) == 1);
 	assert(floor_log2(4) == 2);
 	assert(floor_log2(6) == 2);
 	assert(floor_log2(134) == 7);
+}
+
+static void test_exact_log2() {
+	assert(floor_log2(1) == 0);
+	assert(floor_log2(2) == 1);
+	assert(floor_log2(4) == 2);
+	assert(floor_log2(256) == 8);
+}
+
+void test_math() {
+	test_floor_log2();
+	test_exact_log2();
 
 	assert(hyperfloor(1) == 1);
 	assert(hyperfloor(187) == 128);
