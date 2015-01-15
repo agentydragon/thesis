1172d3209b5ce8350d1277c45aa35f5e5859b925
Michal Pokorný <pok@rny.cz>
2015-01-15 22:44:56 +0100

    Record drilldown times as well as random veb get times

--- src/experiments/veb_performance/veb_performance.c
+++ src/experiments/veb_performance/veb_performance.c
@@ -17,7 +17,7 @@ struct metrics {
 	uint64_t time_nsec;
 };
 
-struct metrics measure_for(uint64_t height, uint64_t N) {
+struct metrics measure_random_for(uint64_t height, uint64_t N) {
 	uint64_t tree_size = 1 << (height - 1);
 	uint64_t* indices = calloc(N, sizeof(uint64_t));
 	for (uint64_t i = 0; i < N; i++) {
@@ -41,14 +41,49 @@ struct metrics measure_for(uint64_t height, uint64_t N) {
 	return tr;
 }
 
+struct metrics measure_drilldown_for(uint64_t height, uint64_t N) {
+	bool* random_decisions = calloc(N * height, sizeof(bool));
+	for (uint64_t i = 0; i < N * height; i++) {
+		random_decisions[i] = rand() % 2;
+	}
+
+	struct measurement measurement = measurement_begin();
+	stopwatch watch = stopwatch_start();
+
+	for (uint64_t i = 0; i < N; i++) {
+		uint64_t node = 0;
+		for (uint64_t j = 1; j < height; j++) {
+			veb_pointer left, right;
+			veb_get_children(node, height, &left, &right);
+
+			assert(left.present && right.present);
+			if (random_decisions[i * height + j]) {
+				node = left.node;
+			} else {
+				node = right.node;
+			}
+		}
+	}
+
+	struct measurement_results results = measurement_end(measurement);
+	struct metrics tr = {
+		.cache_misses = results.cache_misses,
+		.cache_references = results.cache_references,
+		.time_nsec = stopwatch_read_ns(watch)
+	};
+	return tr;
+}
+
 int main(int argc, char** argv) {
 	(void) argc; (void) argv;
-	FILE* output = fopen("experiments/veb_performance/results.csv", "w");
 
-	uint64_t N = 1000000;
+	log_info("measuring accesses to random nodes (i.e. leaf-biased)");
+	FILE* output = fopen("experiments/veb_performance/results-random.csv", "w");
+
+	const uint64_t N = 500000;
 
 	for (uint64_t height = 1; height < 50; height++) {
-		struct metrics results = measure_for(height, N);
+		struct metrics results = measure_random_for(height, N);
 
 		fprintf(output,
 				"%" PRIu64 "\t%" PRIu64 "\t"
@@ -60,5 +95,23 @@ int main(int argc, char** argv) {
 	}
 
 	fclose(output);
+
+	log_info("measuring drilldowns");
+	output = fopen("experiments/veb_performance/results-drilldown.csv", "w");
+
+	for (uint64_t height = 1; height < 50; height++) {
+		struct metrics results = measure_drilldown_for(height, N);
+
+		fprintf(output,
+				"%" PRIu64 "\t%" PRIu64 "\t"
+				"%" PRIu64 "\t%" PRIu64 "\t%" PRIu64 "\t"
+				"\n",
+				height, N,
+				results.cache_misses, results.cache_references, results.time_nsec);
+		fflush(output);
+	}
+
+	fclose(output);
+
 	return 0;
 }
