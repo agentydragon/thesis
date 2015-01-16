acd2e7c8de8bb3da4f5712d7b8b8b3c3b714df96
Michal Pokorný <pok@rny.cz>
2015-01-17 00:00:01 +0100

    Small veb_layout.h cleanup

--- src/veb_layout/veb_layout.h
+++ src/veb_layout/veb_layout.h
@@ -9,22 +9,23 @@ typedef struct veb_pointer {
 	uint64_t node;
 } veb_pointer;
 
+// Primary API
+void veb_get_children(uint64_t node, uint64_t height,
+		veb_pointer* left, veb_pointer* right);
+
+// Used only internally to build cache.
+// node: leaf node index in van Emde Boas order
+// returns: number of the leaf (index from left to right amongst leaves)
+uint64_t veb_get_leaf_index_of_leaf(uint64_t node, uint64_t height);
+
+// Used only in testing.
 void build_veb_layout(uint64_t height,
 		uint64_t node_start,
 		void (*set_node)(void* opaque,
 			uint64_t node, veb_pointer left, veb_pointer right),
 		void* set_node_opaque,
 		veb_pointer leaf_source, uint64_t leaf_stride);
-
 bool veb_is_leaf(uint64_t node, uint64_t height);
-void veb_get_children(uint64_t node, uint64_t height,
-		veb_pointer* left, veb_pointer* right);
-
-// TODO: maybe this one is useless?
 uint64_t veb_get_leaf_number(uint64_t leaf_index, uint64_t height);
-
-// node: leaf node index in van Emde Boas order
-// returns: number of the leaf (index from left to right amongst leaves)
-uint64_t veb_get_leaf_index_of_leaf(uint64_t node, uint64_t height);
 
 #endif
