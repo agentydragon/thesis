8206db939d938d6125e05d2d22fe82902b4954a6
Michal Pokorný <pok@rny.cz>
2015-03-10 12:54:03 +0100

    Add very defensive assertion to toycrypt
    Submission optional.

--- src/dict/test/toycrypt.c
+++ src/dict/test/toycrypt.c
@@ -1,5 +1,7 @@
 #include "dict/test/toycrypt.h"
 
+#include <assert.h>
+
 static uint32_t oneway_fn(uint32_t a, uint64_t b) {
 	// A variant of the FNV hashing function.
 	const uint32_t basis = 2166136261LL, prime = 16777619LL;
@@ -9,6 +11,7 @@ static uint32_t oneway_fn(uint32_t a, uint64_t b) {
 static uint64_t one_round(uint64_t message, uint64_t key) {
 	uint32_t L1 = (message & 0xFFFFFFFF00000000LL) >> 32LL,
 		 L2 = (message & 0x00000000FFFFFFFFLL);
+	assert(message == ((((uint64_t) L1) << 32LL) | (uint64_t) L2));
 	uint32_t R1 = L2 ^ oneway_fn(L1, key), R2 = L1;
 	return (((uint64_t) R1) << 32LL) | (uint64_t) R2;
 }
