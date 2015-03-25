9e2cd78051749f529987c97f39749adf347e2e4f
Michal Pokorný <pok@rny.cz>
2015-03-25 18:39:29 +0100

    Start collecting more counters

--- src/measurement/measurement.c
+++ src/measurement/measurement.c
@@ -22,11 +22,30 @@ typedef struct {
 	int perf_config;
 } counter_spec;
 
+#define CACHE_EVENT(cache,operation,result) (\
+		(PERF_COUNT_HW_CACHE_ ## cache) | \
+		(PERF_COUNT_HW_CACHE_OP_ ## operation << 8) | \
+		(PERF_COUNT_HW_CACHE_RESULT_ ## result << 16))
+
 static counter_spec counters[] = {
 	{"cache_references", PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_REFERENCES},
 	{"cache_misses", PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES},
 	{"branches", PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_INSTRUCTIONS},
 	{"branch_mispredicts", PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES},
+	{"stalled_cycles_frontend", PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_FRONTEND},
+	// Doesn't work (no such file/directory):
+	//   {"stalled_cycles_backend", PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_BACKEND},
+	{"l1d_read_misses", PERF_TYPE_HW_CACHE, CACHE_EVENT(L1D, READ, MISS)},
+	{"l1d_write_misses", PERF_TYPE_HW_CACHE, CACHE_EVENT(L1D, WRITE, MISS)},
+	{"l1i_read_misses", PERF_TYPE_HW_CACHE, CACHE_EVENT(L1I, READ, MISS)},
+	// TODO: prefetches?
+	// Doesn't work (invalid argument):
+	//    {"ll_read_misses", PERF_TYPE_HW_CACHE, CACHE_EVENT(LL, READ, MISS)}
+	//    {"ll_write_misses", PERF_TYPE_HW_CACHE, CACHE_EVENT(LL, WRITE, MISS)}
+	//    {"l1d_read_access", PERF_TYPE_HW_CACHE, CACHE_EVENT(L1D, READ, ACCESS)}
+	//    {"l1d_write_access", PERF_TYPE_HW_CACHE, CACHE_EVENT(L1D, WRITE, ACCESS)},
+	{"dtlb_read_misses", PERF_TYPE_HW_CACHE, CACHE_EVENT(DTLB, READ, MISS)},
+	{"dtlb_write_misses", PERF_TYPE_HW_CACHE, CACHE_EVENT(DTLB, WRITE, MISS)},
 };
 
 struct measurement {
@@ -37,7 +56,7 @@ struct measurement_results {
 	uint64_t* counters;
 };
 
-// TODO: PERF_COUNT_SW_ALIGNMENT_FAULTS
+// TODO: PERF_COUNT_SW_ALIGNMENT_FAULTS, PERF_COUNT_SW_EMULATION_FAULTS
 // TODO: use perf_event groups later?
 
 static int perf_event_open(struct perf_event_attr *hw_event,
@@ -54,13 +73,7 @@ static int event_fd_open(int perf_type, int config) {
 		.exclude_kernel = true,
 		.exclude_hv = true
 	};
-	int fd = perf_event_open(&event, 0, -1, -1, 0);
-
-	if (fd == -1) {
-		log_fatal("cannot open perf_event fd: %s", strerror(errno));
-	}
-
-	return fd;
+	return perf_event_open(&event, 0, -1, -1, 0);
 }
 
 static void event_fd_start(int fd) {
@@ -83,8 +96,13 @@ measurement* measurement_begin() {
 	m->fds = calloc(COUNT_OF(counters), sizeof(int));
 	assert(m->fds);
 	for (uint64_t i = 0; i < COUNT_OF(counters); ++i) {
-		m->fds[i] = event_fd_open(counters[i].perf_type,
+		int fd = event_fd_open(counters[i].perf_type,
 				counters[i].perf_config);
+		if (fd == -1) {
+			log_fatal("cannot open perf_event for %s: %s",
+					counters[i].name, strerror(errno));
+		}
+		m->fds[i] = fd;
 	}
 	for (uint64_t i = 0; i < COUNT_OF(counters); ++i) {
 		event_fd_start(m->fds[i]);
