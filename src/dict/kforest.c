0bca4d6f0a988e3804f9a533365f6efa9784c17d
Michal Pokorný <pok@rny.cz>
2015-03-27 12:06:04 +0100

    Add forgotten dict/kforest

---
+++ src/dict/kforest.c
@@ -0,0 +1,55 @@
+#include "dict/kforest.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include "kforest/kforest.h"
+
+static int8_t init(void** _this, void* args_unused) {
+	(void) args_unused;
+	kforest* this = malloc(sizeof(kforest));
+	if (!this) {
+		return 1;
+	}
+	kforest_init(this);
+	*_this = this;
+	return 0;
+}
+
+static void destroy(void** _this) {
+	if (_this) {
+		kforest_destroy(* (kforest**) _this);
+		free(*_this);
+		*_this = NULL;
+	}
+}
+
+static int8_t find(void* _this, uint64_t key, uint64_t *value, bool *found) {
+	assert(_this);
+	kforest_find(_this, key, value, found);
+	return 0;
+}
+
+static int8_t insert(void* _this, uint64_t key, uint64_t value) {
+	assert(_this);
+	return kforest_insert(_this, key, value);
+}
+
+static int8_t delete(void* _this, uint64_t key) {
+	assert(_this);
+	return kforest_delete(_this, key);
+}
+
+const dict_api dict_kforest = {
+	.init = init,
+	.destroy = destroy,
+
+	.insert = insert,
+	.find = find,
+	.delete = delete,
+
+	.next = NULL,
+	.prev = NULL,
+
+	.name = "dict_kforest"
+};
