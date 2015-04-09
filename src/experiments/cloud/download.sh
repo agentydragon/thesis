ae78b22af02fc60fadfd41340adcc6c5809fe9d4
Michal Pokorný <pok@rny.cz>
2015-04-09 17:28:10 +0200

    Download the entire cloud dataset by default

--- src/experiments/cloud/download.sh
+++ src/experiments/cloud/download.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-wget http://cdiac.ornl.gov/ftp/ndp026c/land_199701_200912/SEP97L.Z
-uncompress SEP97L.Z
+
+cd data
+
+for YEAR in 97 98 99 00 01 02 03 04 05 06 07 08 09; do
+	for MONTH in JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC; do
+		FILE="${MONTH}${YEAR}L"
+		if [ ! -f $FILE ]; then
+			wget http://cdiac.ornl.gov/ftp/ndp026c/land_199701_200912/${FILE}.Z
+			uncompress ${FILE}.Z
+		fi
+	done
+done
